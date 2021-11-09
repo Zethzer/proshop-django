6afe4218e186c60a6a01b1e793687e00819194c8
Zethzer <bernard.y24@gmail.com>
2021-11-09 20:10:29 +0100

    Add update user profile action in the backend

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('register/', views.register_user, name='register'),
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', views.get_users, name='users'),
-    path('profile/', views.get_user_profile, name='users_profile'),
+    path('profile/', views.get_user_profile, name='user_profile'),
+    path('profile/update', views.update_user_profile, name='user_profile_update'),
 ]
