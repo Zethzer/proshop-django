06c0e7df9dd4577e26d41632e16d4808d4ecb8a5
Zethzer <bernard.y24@gmail.com>
2021-11-11 19:16:55 +0100

    Finish shipping screen with actions

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -1,20 +1,26 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from '../components/FormContainer'
-import { register } from '../actions/userActions'
+import { saveShippingAddress } from '../actions/cartActions'
 
 export default function ShippingScreen() {
     const navigate = useNavigate()
-    const [address, setAddress] = useState('')
-    const [city, setCity] = useState('')
-    const [postalCode, setPostalCode] = useState('')
-    const [country, setCountry] = useState('')
+    const dispatch = useDispatch()
+
+    const cart = useSelector(state => state.cart)
+    const { shippingAddress } = cart
+
+    const [address, setAddress] = useState(shippingAddress.address)
+    const [city, setCity] = useState(shippingAddress.city)
+    const [postalCode, setPostalCode] = useState(shippingAddress.postalCode)
+    const [country, setCountry] = useState(shippingAddress.country)
 
     const submitHandler = (e) => {
         e.preventDefault()
-        console.log('Continue !')
+        dispatch(saveShippingAddress({address, city, postalCode, country}))
+        navigate('/payment')
     }
 
     return (
