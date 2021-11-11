e8b5284dc8b9f23e6e0e05090052e58344315293
Zethzer <bernard.y24@gmail.com>
2021-11-12 00:15:05 +0100

    use yarn and add place order screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
 
 export default function App() {
     return (
@@ -24,6 +25,7 @@ export default function App() {
                         <Route path='profile' element={<ProfileScreen/>}/>
                         <Route path='shipping' element={<ShippingScreen/>}/>
                         <Route path='payment' element={<PaymentScreen/>}/>
+                        <Route path='placeorder' element={<PlaceOrderScreen/>}/>
                         <Route path='products/:id' element={<ProductScreen/>}/>
                         <Route path='cart/' element={<CartScreen/>}>
                             <Route path=':id' element={<CartScreen/>}/>
