6439394283e27ac63429f5f7e32111d6797abda9
Zethzer <bernard.y24@gmail.com>
2021-11-11 19:33:29 +0100

    Add CheckoutSteps component

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -4,6 +4,7 @@ import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from '../components/FormContainer'
 import { saveShippingAddress } from '../actions/cartActions'
+import CheckoutSteps from '../components/CheckoutSteps'
 
 export default function ShippingScreen() {
     const navigate = useNavigate()
@@ -25,6 +26,7 @@ export default function ShippingScreen() {
 
     return (
         <FormContainer>
+            <CheckoutSteps step1 step2 />
             <h1>Shipping</h1>
             <Form onSubmit={submitHandler}>
                 <Form.Group className='mb-3' controlId='address'>
