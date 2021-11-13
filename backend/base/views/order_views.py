6090a16b2b49d50cbf2cff092ef7778286512291
Zethzer <bernard.y24@gmail.com>
2021-11-13 16:36:29 +0100

    Add update payment informations for order in backend

--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework import status
+from datetime import datetime
 
 from ..models import Product, Order, OrderItem, ShippingAddress
 from ..serializers import OrderSerializer
@@ -65,3 +66,15 @@ def get_order_by_id(request, pk):
             Response({'detail': 'Not authorized to view this order'}, status=status.HTTP_400_BAD_REQUEST)
     except Exception as e:
         return Response({'detail': 'Order does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def update_order_to_paid(request, pk):
+    order = Order.objects.get(_id=pk)
+
+    order.isPaid = True
+    order.paidAt = datetime.now()
+    order.save()
+
+    return Response('Order was paid')
