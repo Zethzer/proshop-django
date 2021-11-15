62d359291dd3fad21b46beb05dc1d9988502c563
Zethzer <bernard.y24@gmail.com>
2021-11-15 16:04:56 +0100

    Create and update product by admin endpoints in backend

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -4,5 +4,7 @@ from ..views import product_views as views
 urlpatterns = [
     path('', views.get_products, name='products'),
     path('<str:pk>', views.get_product, name='product'),
+    path('create/', views.create_product, name='create_product'),
+    path('update/<str:pk>/', views.update_product, name='update_product'),
     path('delete/<str:pk>/', views.delete_product, name='delete_product'),
 ]
