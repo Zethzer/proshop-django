43acd40e42e3053d5f420f2db2ef46181475a52c
Zethzer <bernard.y24@gmail.com>
2021-11-17 19:05:53 +0100

    Add get top products endpoint

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.get_products, name='products'),
     path('create/', views.create_product, name='create_product'),
     path('upload/', views.upload_image, name='image_upload'),
+    path('top/', views.get_top_products, name='top_products'),
     path('<str:pk>/reviews/', views.create_product_review, name='create_review'),
     path('<str:pk>', views.get_product, name='product'),
     path('update/<str:pk>/', views.update_product, name='update_product'),
